45f2215360c00afdbb6e6f8cec3e1ce5b2b4aaea
Pablo <pablomoreirasantos.hp@gmail.com>
2025-04-13 18:39:15 -0300

    feat: Autentificação finalizada

--- AutentificacaoApi/obj/Debug/net8.0/AutentificacaoApi.AssemblyInfo.cs
+++ AutentificacaoApi/obj/Debug/net8.0/AutentificacaoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutentificacaoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d629db337cf6927aa36f9e93d90147999ccf94f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ca4880b257202cb98dc5dcbfb6ac958a308f02c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutentificacaoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutentificacaoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
